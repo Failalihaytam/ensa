ddcffcb33938677ab51fff54c6c0e3c70ff4803d
Haytham Failali Jatabi <93624059+Failalihaytam@users.noreply.github.com>
2024-02-14 22:02:43 +0100

    rename data to automate

--- THL/main.c
+++ THL/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdint.h>
 
 #define MAX_LINES 1000
@@ -6,112 +7,125 @@
 
 typedef struct
 {
-    int depart;
-    int arrive;
-    char etiquete;
+    int depart;      // État de départ de la transaction
+    int arrive;      // État d'arrivée de la transaction
+    char etiquete;   // Étiquette de la transaction
 } Transaction;
 
 typedef struct
 {
-    Transaction transactions[MAX_LINES];
-    int first;
-    int last;
-} Data;
+    Transaction transactions[MAX_LINES]; // Tableau de transactions
+    int first;                           // État initial
+    int last;                            // État final
+} Automate; // Structure représentant un automate
 
-// declarer les prototypes des fonctions
-Data stocker(FILE *file);
-void alphabet(Data data);
-void menu(Data data);
+// Déclaration des prototypes de fonctions
+Automate stocker(FILE *file); // Fonction pour stocker les données de transaction
+void alphabet(Automate automate); // Fonction pour afficher les alphabets
+void menu(Automate automate); // Fonction pour afficher le menu
 
 int transactions_index = 0;
 
-int main(void)
+int main()
 {
-    FILE *my_file = fopen("projet.txt", "r");
+    FILE *my_file = fopen("projet.txt", "r"); // Ouvre le fichier "projet.txt" en mode lecture
     if (my_file == NULL)
     {
-        return 1;
+        return 1; // Quitte le programme si le fichier n'a pas pu être ouvert
     }
-    Data my_data = stocker(my_file);
+    Automate my_automate = stocker(my_file); // Stocke les données du fichier dans un automate
+    fclose(my_file); // Ferme le fichier
 
-    // afficher le menu des operations
-    menu(my_data);
+    // Affiche le menu des opérations
+    menu(my_automate);
 
     return 0;
 }
 
-Data stocker(FILE *file)
+Automate stocker(FILE *file)
 {
-    Data data;
+    Automate automate; // Déclare une variable de type Automate pour stocker les données
+    char line[MAX_LENGTH]; // Déclare un tableau de caractères pour stocker chaque ligne lue du fichier
 
-    char line[MAX_LENGTH];
-
-    while (fgets(line, sizeof(line), file) != 0)
+    while (fgets(line, sizeof(line), file) != 0) // Lit chaque ligne du fichier
     {
-        if (sscanf(line, "%d %d %c", &data.transactions[transactions_index].depart,
-                &data.transactions[transactions_index].arrive,
-                &data.transactions[transactions_index].etiquete) == 3)
+        if (sscanf(line, "%d %d %c", &automate.transactions[transactions_index].depart,
+                   &automate.transactions[transactions_index].arrive,
+                   &automate.transactions[transactions_index].etiquete) == 3) // Si la lecture est réussie pour une transaction
         {
-            transactions_index++;
+            transactions_index++; // Incrémente l'index des transactions
         }
-        else
+        else // Sinon, cela signifie qu'on a atteint la ligne contenant les états initial et final
         {
-            sscanf(line, "%d", &data.first);
-            fgets(line, sizeof(line), file);
-            sscanf(line, "%d", &data.last);
-            break;
+            sscanf(line, "%d", &automate.first); // Lit l'état initial
+            fgets(line, sizeof(line), file);     // Passe à la ligne suivante pour lire l'état final
+            sscanf(line, "%d", &automate.last);  // Lit l'état final
+            break; // Sort de la boucle
         }
     }
-    fclose(file);
 
-    return data;
+    return automate; // Retourne la structure Automate avec les données lues
 }
 
-void alphabet(Data data)
+void alphabet(Automate automate)
 {
     printf("Alphabets:\n");
     for (int i = 0; i < transactions_index; ++i)
     {
-        printf("%c\n", data.transactions[i].etiquete);
+        printf("%c ", automate.transactions[i].etiquete); // Affiche l'étiquette de chaque transaction
     }
+    printf("\n");
     return;
 }
 
-void menu(Data data)
+void menu(Automate automate)
 {
     int answer;
     do
     {
+        char input[100];
         do
         {
-            printf("entrer un nombre pour choisir votre operation: \n");
-            printf("1- Afficher la liste des transaction\n2- Afficher l'etat initial\n3- Afficher l'etat final\n4- Afficher la liste des alphabets\n5- Quitter le programme\n");
-            scanf("%i", &answer);
-        }
-        while (answer < 1 || answer > 5);
+            printf("Entrez un nombre pour choisir votre opération :\n");
+            printf("1- Afficher la liste des transactions\n2- Afficher l'état initial\n3- Afficher l'état final\n4- Afficher la liste des alphabets\n5- Quitter le programme\n");
+
+            // Lit l'entrée de l'utilisateur sous forme de chaîne de caractères
+            scanf("%s", input);
+
+            // Convertit l'entrée en entier à l'aide de atoi
+            answer = atoi(input);
+
+            // Vérifie si l'entrée est un entier valide
+            if (answer < 1 || answer > 5)
+            {
+                printf("Entrée invalide. Veuillez entrer un nombre entre 1 et 5.\n");
+            }
+        } while (answer < 1 || answer > 5);
 
         if (answer == 1)
         {
             printf("Transactions:\n");
             for (int i = 0; i < transactions_index; ++i)
             {
-                printf("%d %d %c\n", data.transactions[i].depart, data.transactions[i].arrive, data.transactions[i].etiquete);
+                printf("(%d, %c) -> %d\n", automate.transactions[i].depart, automate.transactions[i].etiquete, automate.transactions[i].arrive); // Affiche chaque transaction
             }
         }
         else if (answer == 2)
         {
-            printf("L'etat initial: %d\n", data.first);
+            printf("État initial : %d\n", automate.first); // Affiche l'état initial
         }
         else if (answer == 3)
         {
-            printf("L'etat final: %d\n", data.last);
+            printf("État final : %d\n", automate.last); // Affiche l'état final
         }
         else if (answer == 4)
         {
-            alphabet(data);
+            alphabet(automate); // Affiche les alphabets
         }
+        else
+            printf("Impossible");
+
         printf("\n");
-    }
-    while (answer != 5);
+    } while (answer != 5); // Continue tant que l'utilisateur ne choisit pas de quitter
     return;
 }
