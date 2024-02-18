b3f1c240cd8b4c392f7de82f4059ddc95f91d3c9
Haytham Failali Jatabi <93624059+Failalihaytam@users.noreply.github.com>
2024-02-18 20:22:16 +0000

    Sun, Feb 18, 2024, 9:22 PM +01:00

---
+++ main.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <ctype.h>
+
+#define MAX_TRANSITIONS 100
+#define MAX_ALPHABETS 26
+
+
+typedef struct {
+    int etat_depart;
+    int etat_arrive;
+    char etiquette;
+} Transition;
+
+typedef struct {
+    Transition transitions[MAX_TRANSITIONS];
+    int nb_transitions;
+    int etats_initiaux[MAX_TRANSITIONS];
+    int nb_etats_initiaux;
+    int etats_finaux[MAX_TRANSITIONS];
+    int nb_etats_finaux;
+} Automate;
+
+void count_lines(FILE *file, Automate *automate);
+void last_lines(FILE *file, Automate *automate);
+
+
+
+void lireAutomate(Automate *automate, const char *nom_fichier) {
+    FILE *fichier = fopen(nom_fichier, "r");
+
+    if (fichier == NULL) {
+        perror("Erreur lors de l'ouverture du fichier");
+        exit(EXIT_FAILURE);
+    }
+
+    while ((fscanf(fichier, "%d %d %c", &automate->transitions[automate->nb_transitions].etat_depart,
+                                         &automate->transitions[automate->nb_transitions].etat_arrive,
+                                         &automate->transitions[automate->nb_transitions].etiquette) == 3)
+                                        && isalpha(automate->transitions[automate->nb_transitions].etiquette))
+    {
+
+        automate->nb_transitions++;
+        if (automate->nb_transitions >= MAX_TRANSITIONS) {
+            fprintf(stderr, "Trop de transitions. Ajustez la valeur de MAX_TRANSITIONS.\n");
+            fclose(fichier);
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    fclose(fichier);
+}
+
+void afficherAutomate(const Automate *automate) {
+    printf("Transitions de l'automate :\n");
+    for (int i = 0; i < automate->nb_transitions; ++i) {
+        printf("%d -> %d : %c\n", automate->transitions[i].etat_depart,
+                                   automate->transitions[i].etat_arrive,
+                                   automate->transitions[i].etiquette);
+
+    }
+}
+
+int main() {
+    Automate automate;
+    afficherAutomate(automate);
+}
